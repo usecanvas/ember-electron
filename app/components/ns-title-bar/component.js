2c9d7441def0dc80e899b285f8ad51fd3c4bceab
Max Schoening <mail@maximilian.io>
2015-06-03 18:42:42 -0700

    Rename ns-titlebar to ns-title-bar
    Good to follow Cocoa conventions where it's TitleBar not Titlebar

---
+++ app/components/ns-title-bar/component.js
@@ -0,0 +1,5 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  classNames: ['ns-title-bar', 'layout-horizontal', 'layout-between-justified']
+});
--- app/components/ns-titlebar/component.js
+++
@@ -1,5 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-  classNames: ['ns-titlebar', 'layout-horizontal', 'layout-between-justified']
-});
